ca765dae7d4a2da30eda23732e262829649af5bf
Joanna Monserrat Espinoza Sánchez <ejoannamonserrat@gmail.com>
2021-10-28 20:10:00 -0500

    AccederAlaVistaDesdeKotlin

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var message: String = findViewById<TextView>(R.id.textView).text.toString()
+        log.i(tag:"message textview", message)
     }
 }
